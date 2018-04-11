de19b34f98023a70679f31884ccea0a131d2e253
dae najda <dawidek199710@wp.pl>
2018-04-11 18:39:03 +0200

    Add models(News,Interested,Declaration,Comments)

--- NewsCenter/NewsCenter/app/models.py
+++ NewsCenter/NewsCenter/app/models.py
@@ -5,3 +5,20 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length = 32)
+    date = models.DateTimeField()
+    description = models.TextField()
+    address = models.CharField(max_length = 32)
+
+class Interested(models.Model):
+    id_user = models.ForeignKey(User)
+    id_news = models.ForeignKey(News)
+
+class Declaration(models.Model):
+    id_user = models.ForeignKey(User)
+    id_news = models.ForeignKey(News)
+
+class Comments(models.Model):
+    id_comment = models.ForeignKey(Comment)
+    id_news = models.ForeignKey(News)

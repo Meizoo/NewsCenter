f003d47b1de36088cec12a9e571f91b7bbebe0b6
Patryk Milewski <mpatryk97@gmail.com>
2018-04-11 18:43:22 +0200

    removed redundancy

--- NewsCenter/NewsCenter/app/models.py
+++ NewsCenter/NewsCenter/app/models.py
@@ -4,7 +4,15 @@ Definition of models.
 
 from django.db import models
 
-# Create your models here.
+STATE = ( # todo rewrite into Models.field
+		(1, 'BANNED'),
+		(2, 'GUEST'),
+		(3, 'USER'),
+		(4, 'MODERATOR'),
+		(5, 'ADMINISTRATOR'),
+		(6, 'HEAD_ADMINISTRATOR'),	
+	)
+
 class News(models.Model):
     title = models.CharField(max_length = 32)
     date = models.DateTimeField()
@@ -19,18 +27,9 @@ class Declaration(models.Model):
     id_user = models.ForeignKey(User)
     id_news = models.ForeignKey(News)
 
-class Comments(models.Model):
+class CommentNews(models.Model):
     id_comment = models.ForeignKey(Comment)
     id_news = models.ForeignKey(News)
-
-STATE = ( # todo rewrite into Models.field
-		(1, 'BANNED'),
-		(2, 'GUEST'),
-		(3, 'USER'),
-		(4, 'MODERATOR'),
-		(5, 'ADMINISTRATOR'),
-		(6, 'HEAD_ADMINISTRATOR'),	
-	)
 
 class Association(models.Model):
 	name = models.CharField(max_length=32)
@@ -45,9 +44,6 @@ class User(models.Model):
 	state = models.CharField(max_length=1, choices=STATE)
 
 class Comment(models.Model):
-	user_id = models.ForeignKey(User)
+	id_user = models.ForeignKey(User)
 	comment = models.CharField(max_length=128)
 
-class NewsComment(models.Model):
-	news_id = models.ForeignKey(News)
-	user_id = models.ForeignKey(User)

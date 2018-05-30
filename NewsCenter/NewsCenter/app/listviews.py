b3c6438f84dc55331b3780f1b2120b28627ed6fc
Patryk Milewski <mpatryk97@gmail.com>
2018-05-30 20:21:08 +0000

    listview refactor

--- NewsCenter/NewsCenter/app/listviews.py
+++ NewsCenter/NewsCenter/app/listviews.py
@@ -7,8 +7,8 @@ import math
 
 class ArticleListView(generic.ListView):
 	def __init__(self):
+		row = 3
 		self.querytable = [[]]
 		queryset = News.objects.all()
 		for i in range(0, math.ceil(len(queryset) / row)):
 			self.querytable.append([queryset[i * row + j] for j in range(0, row) if i * row + j < len(queryset)])
-		row = 3
